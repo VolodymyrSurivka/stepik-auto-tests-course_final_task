062f72400f340c6a4035f90f8de13ba1bb4d3dba
VolodymyrSurivka <volodymyrsurivka@gmail.com>
2020-09-24 11:24:39 +0300

    Requirements file edited

--- conftest.py
+++ conftest.py
@@ -3,13 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-# @pytest.fixture(scope="function")
-# def browser():
-#     browser = webdriver.Chrome()
-#     yield browser
-#     browser.quit()
-
-
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
